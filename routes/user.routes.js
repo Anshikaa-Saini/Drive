f4badd96d51b17a27b215594fb67c0378a9dbf72
Anshika Saini <143860130+Anshikaa-Saini@users.noreply.github.com>
2025-08-28 12:14:45 +0000

    encryption

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
 
 router.get('/register', (req, res) => {
     res.render('register');
@@ -22,14 +23,53 @@ router.post('/register',
 
     const { username, email, password } = req.body;
 
+    const hashPassword = await bcrypt.hash(password, 10);
+
     const newUser = await userModel.create({ 
         username, 
         email, 
-        password 
+        password : hashPassword
     });
 
     res.json(newUser);
    
 })
 
+router.get('/login', (req, res)=> {
+    res.render('login');
+})
+
+router.post('/login', body('username').trim().isLength({min: 3}),
+body('password').trim().isLength({min: 5}),
+async(req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            error: errors.array(),
+            message: 'Invalid data'
+        })
+    }
+
+    const { username, password } = req.body;
+
+    const user= await userModel.findOne({
+        username: username
+    })
+
+    if(!user) {
+        return res.status(400).json({
+            message: 'username or password is incorrect'
+    })
+}
+
+const isMatch = await bcrypt.compare(password, user.password)
+
+if(!isMatch) {
+    return res.status(400).json({
+        message: 'username or password is incorrect'
+    })
+}
+
+})
 module.exports = router;
